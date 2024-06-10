68fc04a1122e24f63befffcb710b63482323220d
Daniel Smith <zamadatix@gmail.com>
2024-06-10 23:23:46 +0000

    Update promox-autoinstall-stage2.sh

--- proxmox/promox-autoinstall-stage2.sh
+++ proxmox/promox-autoinstall-stage2.sh
@@ -1,3 +1,58 @@
-#!/bin/bash
+# Script variables
+# The password as encrypted in /etc/shadow
+PASSWORD='$y$j9T$h..kRJ8t1N.BvqSVwFbCz.$oQPkVsHO5dtXQlqN3IMKGYeg1o4.wIaT8husYlOs76B:19884:0:99999:7:::'
 
-echo "hello world" > /tmp/test.txt
+# Create the stepcg user in linux PAM and then add it to the proxmox gui administrators
+useradd -m -s /bin/bash stepcg
+usermod -aG sudo stepcg
+pveum useradd stepcg@pam -comment "StepCG User"
+pveum aclmod / -user stepcg@pam -role Administrator
+
+# Set the passwords for both root and stepcg
+sed -i "s/^stepcg:.*/stepcg:$PASSWORD/" /etc/shadow
+sed -i "s/^root:.*/root:$PASSWORD/" /etc/shadow
+
+# Disable the enterprise repos
+sed -i "s/^/#/" /etc/apt/sources.list.d/pve-enterprise.list
+sed -i "s/^/#/" /etc/apt/sources.list.d/ceph.list
+echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
+echo "deb http://download.proxmox.com/debian/pve bookworm pve-no-subscription" >> /etc/apt/sources.list
+
+# Remove nag message
+wget -qO- https://raw.githubusercontent.com/foundObjects/pve-nag-buster/master/install.sh | bash
+
+# Run updates
+apt-get update
+apt-get upgrade -y
+pveam update
+
+# Grab the latest Ubuntu 24.04 container template
+pveam download local $(pveam available | grep ubuntu-24.04-standard | awk '/^system/ {print $2}')
+
+# Set some sysctls for better networking
+cat << 'EOF' > /etc/sysctl.d/net.conf
+net.core.default_qdisc = fq
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.core.default_qdisc = cake
+
+net.ipv4.ip_forward = 1
+net.ipv4.ip_local_port_range = 30000 65535
+net.ipv4.tcp_congestion_control = bbr
+net.ipv4.tcp_fastopen = 3
+net.ipv4.udp_rmem_min = 32768
+net.ipv4.udp_wmem_min = 32768
+
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.ip_local_port_range = 30000 65535
+net.ipv6.tcp_congestion_control = bbr
+net.ipv6.tcp_fastopen = 3
+net.ipv6.udp_rmem_min = 32768
+net.ipv6.udp_wmem_min = 32768
+net.ipv6.conf.all.accept_ra = 2
+net.ipv6.conf.default.accept_ra = 2
+net.ipv6.conf.vmbr0.accept_ra = 2
+EOF
+
+# Clear out the boot time script
+rm -f /root/proxmox-autoinstall-stage1.sh /etc/cron.d/proxmox-autoinstall-stage1
